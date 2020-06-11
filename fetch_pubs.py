32b08d7e11764a2941719fe8e915a8da6baa8f86
Panos Ipeirotis <ipeirotis@gmail.com>
2020-06-11 18:24:24 +0000

    Modified to avoid fill, and use latest scholarly

--- fetch_pubs.py
+++ fetch_pubs.py
@@ -28,14 +28,14 @@ except Exception as e:
 # Convert Author object to a dictionary, to allow JSON serialization
 author_dict = dict(author.__dict__)
 # Add last-updated information in the dictionary
-author_dict['last_updated_ts'] = timestamp
-author_dict['last_updated'] = date_str
+author_dict["last_updated_ts"] = timestamp
+author_dict["last_updated"] = date_str
 # Remove the publications and co-author entries, which are not needed in the JSON
-del author_dict['publications']
-del author_dict['coauthors']
-del author_dict['nav']
-del author_dict['_sections']
-del author_dict['_filled']
+del author_dict["publications"]
+del author_dict["coauthors"]
+del author_dict["nav"]
+del author_dict["_sections"]
+del author_dict["_filled"]
 
 # Save the author profile in a JSON file
 with open(author_name + ".json", "w") as f:
@@ -43,36 +43,36 @@ with open(author_name + ".json", "w") as f:
 
 # Go through the publicationw now, and fill them in with their details
 # tqdm just to know where we are and how long it will take (often 15-20 mins for ~100 pubs)
-for pub in tqdm.tqdm(author.publications):
-    try:
-        if not pub._filled:
-            pub.fill()
-    except Exception as e:
-        pass
+# for pub in tqdm.tqdm(author.publications):
+#     try:
+#        if not pub._filled:
+#            pub.fill()
+#    except Exception as e:
+#        pass
 
 # Once we have the fully complete publications, we convert them to dictionaries to
 # be able to serialize them in JSON
 publications = []
 for pub in author.publications:
     pub_dict = dict(pub.__dict__)
-    del pub_dict['nav']
+    del pub_dict["nav"]
     publications.append(pub_dict)
 
 # Bookkeeping with publications, ensuring that everything is serializable
 for p in publications:
     # The "abstract" is often a BS4.Tag, so we need to convert to string
-    if 'abstract' in p['bib']:
-        p['bib']['abstract'] = str(p['bib']['abstract'])
+    if "abstract" in p["bib"]:
+        p["bib"]["abstract"] = str(p["bib"]["abstract"])
 
     # Add entries for cites_per_year and citedby, if they are not in the response
-    if 'cites_per_year' in p:
-        p['cites_per_year'] = json.loads(json.dumps(dict(p['cites_per_year'])))
-    else:
-        p['cites_per_year'] = dict()
-    p['citedby'] = p['citedby'] if 'citedby' in p else 0
-    p['last_updated_ts'] = timestamp
-    p['last_updated'] = date_str
+    # if 'cites_per_year' in p:
+    #    p['cites_per_year'] = json.loads(json.dumps(dict(p['cites_per_year'])))
+    # else:
+    #    p['cites_per_year'] = dict()
+    p["citedby"] = p["bib"]["cites"] if "cites" in p["bib"] else 0
+    p["last_updated_ts"] = timestamp
+    p["last_updated"] = date_str
 
 # Save the publications in a JSON file
 with open(author_name + "_pubs.json", "w") as f:
-    json.dump(publications, f)
+    json.dump(publications, f, indent=4)
