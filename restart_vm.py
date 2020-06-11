8ae5f53cf19dff96b27237ce22713a538bb9e6e1
Panos Ipeirotis <ipeirotis@gmail.com>
2020-06-11 18:34:19 +0000

    fixing Foster

--- restart_vm.py
+++ restart_vm.py
@@ -4,16 +4,16 @@ import pprint
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google-credentials-scholarAPI.json'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-credentials-scholarAPI.json"
 credentials = GoogleCredentials.get_application_default()
-service = discovery.build('compute', 'v1', credentials=credentials)
+service = discovery.build("compute", "v1", credentials=credentials)
 
 # Project ID for this request.
-project = 'scholarapi'
+project = "scholarapi"
 # The name of the zone for this request.
-zone = 'us-east1-b'
+zone = "us-east1-b"
 # Name of the instance resource to start.
-instance = 'web'
+instance = "web"
 
 # Reset the instance
 request = service.instances().reset(project=project, zone=zone, instance=instance)
--- upload_to_google_storage.py
+++ upload_to_google_storage.py
@@ -22,16 +22,14 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob.upload_from_filename(source_file_name)
     blob.make_public()
 
-    print('File {} uploaded to {}.'.format(
-        source_file_name,
-        destination_blob_name))
+    print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
 
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google-credentials-scholarAPI.json'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-credentials-scholarAPI.json"
 storage_client = storage.Client()
 
-upload_blob('publications_scholar', 'ipeirotis.json', 'ipeirotis.json')
-upload_blob('publications_scholar', 'ipeirotis_pubs.json', 'ipeirotis_pubs.json')
+upload_blob("publications_scholar", "ipeirotis.json", "ipeirotis.json")
+upload_blob("publications_scholar", "ipeirotis_pubs.json", "ipeirotis_pubs.json")
 
-upload_blob('publications_scholar', 'provost.json', 'provost.json')
-upload_blob('publications_scholar', 'provost_pubs.json', 'provost_pubs.json')
+upload_blob("publications_scholar", "foster provost.json", "provost.json")
+upload_blob("publications_scholar", "foster provost_pubs.json", "provost_pubs.json")
