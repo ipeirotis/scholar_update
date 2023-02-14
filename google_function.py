994aad8c1c7701462aba17b85534910776dbf4ba
Panos Ipeirotis <ipeirotis@gmail.com>
2023-02-14 14:52:56 -0500

    Update google_function.py
    * Added status codes to the return values of the functions to indicate success or failure.
    * Used get method instead of manual conditional checks to retrieve the author_name value.
    * Moved logging statements to the except block to properly log the exception messages.
    * Changed the function name api_call to be more descriptive, such as update_scholar_profile.
    * Renamed the msg variable to message for better readability.
    * refactor scholar_update and break it into two pieces: one that querying scholarly to get the author and publications objects, and one that stores the author and publications objects as json on Google bucket
    * instead of using the parameter "author_name" to define the name of the file in the bucket, added one more parameter in the HTTP call (say filename) that will be used to define the name of the file stored in the bucket

--- google_function.py
+++ google_function.py
@@ -1,53 +1,49 @@
 import functions_framework
-
 import json
 import logging
 from datetime import datetime
-
 from google.cloud import storage
 from scholarly import scholarly
 
 @functions_framework.http
-def api_call(request):
+def update_scholar_profile(request):
     """HTTP Cloud Function.
     Args:
        request (flask.Request): The request object.
-       <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
     Returns:
        The response text, or any set of values that can be turned into a
-       Response object using `make_response`
-       <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+       Response object using `make_response`.
     """
     request_json = request.get_json(silent=True)
     request_args = request.args
 
-    if request_json and "author_name" in request_json:
-        author_name = request_json["author_name"]
-    elif request_args and "author_name" in request_args:
-        author_name = request_args["author_name"]
-    else:
-        return "Missing author name"
+    author_name = request_json.get("author_name", request_args.get("author_name"))
+    filename = request_json.get("filename", request_args.get("filename"))
+    if not author_name or not filename:
+        return "Missing author name or filename", 400
 
-    msg = scholar_update(author_name)
+    author, publications = get_scholar_data(author_name)
+    if author is None or publications is None:
+        return "Error getting data from Google Scholar", 500
 
-    return msg
-    
+    result = store_data_on_bucket(filename, author, publications)
+    if result is None:
+        return "Error storing data on Google Bucket", 500
 
-def scholar_update(author_name):
-    # We want to keep track of the last time we updated the file
-    now = datetime.now()
-    timestamp = int(datetime.timestamp(now))
-    date_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    return f"Updated entry for author {author_name} with filename {filename}", 200
 
-    # Query for author and fill in the details
+def get_scholar_data(author_name):
     try:
         search_query = scholarly.search_author(author_name)
         author = scholarly.fill(next(search_query))
     except Exception:
-        logging.exception("Something went wrong when searching Google Scholar")
-        return "Something went wrong when searching Google Scholar"
+        logging.exception("Error getting data from Google Scholar")
+        return None, None
 
-    # Bookkeeping with publications
+    now = datetime.now()
+    timestamp = int(datetime.timestamp(now))
+    date_str = now.strftime("%Y-%m-%d %H:%M:%S")
+
     publications = []
     for pub in author["publications"]:
         pub["citedby"] = pub.pop("num_citations")
@@ -55,29 +51,27 @@ def scholar_update(author_name):
         pub["last_updated"] = date_str
         publications.append(pub)
 
-    # Add last-updated information in the dictionary
     author["last_updated_ts"] = timestamp
     author["last_updated"] = date_str
-    # Remove the publications entries, which are not needed in the JSON
     del author["publications"]
 
+    return author, publications
+
+def store_data_on_bucket(filename, author, publications):
     try:
-        # Setup access to the Google Cloud Storage bucket
         client = storage.Client()
         bucket_name = "publications_scholar"
         bucket = client.bucket(bucket_name)
 
-        # Save the author profile in a JSON file
-        filename = f"{author_name}.json"
-        blob = bucket.blob(str(filename))
+        author_filename = f"{filename}.json"
+        blob = bucket.blob(str(author_filename))
         blob.upload_from_string(json.dumps(author), content_type="application/json")
 
-        # Save the publications in a JSON file
-        filename = f"{author_name}_pubs.json"
-        blob = bucket.blob(str(filename))
+        publications_filename = f"{filename}_pubs.json"
+        blob = bucket.blob(str(publications_filename))
         blob.upload_from_string(json.dumps(publications), content_type="application/json")
     except Exception:
-        logging.exception("Something went wrong when writing to the bucket")
-        return "Something went wrong when writing to the bucket"
+        logging.exception("Error storing data on Google Bucket")
+        return None
 
-    return f"Updated entry for author {author_name}"
+    return True
