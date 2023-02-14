3fb9a012340ed44b89069420c94bdbe4e68bad1d
Panos Ipeirotis <ipeirotis@gmail.com>
2023-02-14 17:03:24 -0500

    Delete decrypt_secret.sh

--- decrypt_secret.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSWORD" \
---output google-credentials-scholarAPI.json google-credentials-scholarAPI.json.gpg
